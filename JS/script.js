7ba80853cc9f49fa15e303e3718699ee221f0d53
magomed <alievmagomed1990@mail.ru>
2021-10-06 19:57:23 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// let incr = 14;
+// let decr = 69;
+
+
+// ++incr;
+// --decr;
+
+
+// console.log(incr);
+// console.log(decr);
+
+
+const myCode = false,
+      youCode = false;
+
+console.log(myCode ||!youCode);
